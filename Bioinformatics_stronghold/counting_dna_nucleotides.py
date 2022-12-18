5b13c6b8d433f5016b3a4d91de34088f6f916470
Rah-T <117505290+Rah-T@users.noreply.github.com>
2022-12-18 21:24:52 +0530

    Update counting_dna_nucleotides.py

--- Bioinformatics_stronghold/counting_dna_nucleotides.py
+++ Bioinformatics_stronghold/counting_dna_nucleotides.py
@@ -1,3 +1,3 @@
-f=open(r"C:\SelfLearn\Rosalind\Bioinformatics_stronghold\rosalind_dna.txt",'r')
+f=open(r"rosalind_dna.txt",'r')
 f=f.read()
 print(f.count("A"),"\t",f.count("C"),"\t",f.count("G"),"\t",f.count("T"))
